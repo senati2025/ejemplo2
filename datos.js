e6925ef67845b20224911b44fd67154a7d42653b
reii-sr <rsr.reiise@gmail.com>
2025-04-07 17:39:45 -0500

    data2
    commit

--- datos.js
+++ datos.js
@@ -1,40 +1,44 @@
 // Lista de usuarios (puedes agregar más)
 const usuarios = [
-    {
-      nombre: "ROJAS CHIROQUE GIACOMO",
-      direccion: "UCV 35 LOTE 32 HUAYCAN",
-      id: 1518271
-    },
-    {
-      nombre: "Maria Fatima",
-      direccion: "Mz K lote 3",
-      id: 1548358
-    },
-    {
-      nombre: "Karel de la torre",
-      direccion: "Ancón ",
-      id: 1534541
-    }
-    {
-      nombre: "Piero Javier Chacpi Tahua",
-      direccion: "Calle Limoncillos 102 lote 16",
-      id: 1540314
-    }
-  ];
-  
-  // Mostrar usuarios en el HTML
-  const resultadosContainer = document.getElementById("resultadosContainer");
-  
-  usuarios.forEach(usuario => {
-    const item = document.createElement("div");
-    item.classList.add("resultado-item");
-  
-    item.innerHTML = `
+  {
+    nombre: "Reiser Soplin Ramos",
+    direccion: "Av. Tupac Amaru km 23",
+    id: 1522611
+  },
+  {
+    nombre: "ROJAS CHIROQUE GIACOMO",
+    direccion: "UCV 35 LOTE 32 HUAYCAN",
+    id: 1518271
+  },
+  {
+    nombre: "Maria Fatima",
+    direccion: "Mz K lote 3",
+    id: 1548358
+  },
+  {
+    nombre: "Karel de la torre",
+    direccion: "Ancón ",
+    id: 1534541
+  },
+  {
+    nombre: "Piero Javier Chacpi Tahua",
+    direccion: "Calle Limoncillos 102 lote 16",
+    id: 1540314
+  }
+];
+
+// Mostrar usuarios en el HTML
+const resultadosContainer = document.getElementById("resultadosContainer");
+
+usuarios.forEach(usuario => {
+  const item = document.createElement("div");
+  item.classList.add("resultado-item");
+
+  item.innerHTML = `
       <p><strong>Nombre:</strong> ${usuario.nombre}</p>
       <p><strong>Dirección:</strong> ${usuario.direccion}</p>
       <p><strong>ID:</strong> ${usuario.id}</p>
     `;
-  
-    resultadosContainer.appendChild(item);
-  });
-  
+
+  resultadosContainer.appendChild(item);
+});
