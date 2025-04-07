5d40ff96845ed5b2aede4d5d15fe19cfe476eb77
reii-sr <rsr.reiise@gmail.com>
2025-04-07 17:08:41 -0500

    data2
    commit

---
+++ datos.js
@@ -0,0 +1,32 @@
+const personas = [
+    {
+      nombre: "Ana Pérez",
+      direccion: "Calle Las Flores 123, Lima",
+      id: "A001"
+    },
+    {
+      nombre: "Carlos López",
+      direccion: "Avenida Principal 456, Miraflores",
+      id: "B002"
+    },
+    {
+      nombre: "Sofía Gómez",
+      direccion: "Jirón Los Pinos 789, San Isidro",
+      id: "C003"
+    },
+    {
+      nombre: "Mateo Vargas",
+      direccion: "Pasaje La Luna 101, Barranco",
+      id: "D004"
+    },
+    {
+      nombre: "Isabela Torres",
+      direccion: "Boulevard del Mar 222, Chorrillos",
+      id: "E005"
+    }
+  ];
+  
+  // Puedes opcionalmente agregar una función para acceder a los datos desde el index.html
+  function obtenerDatosPersonas() {
+    return personas;
+  }
